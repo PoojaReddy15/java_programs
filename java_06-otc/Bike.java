ec4383e415b93a98a096d00e264a584749762a60
PoojaReddy1215 <poojareddy1215@gmail.com>
2021-10-06 10:13:01 +0530

    todays task

---
+++ java_06-otc/Bike.java
@@ -0,0 +1,7 @@
+class Bike{
+  public static boolean start(){
+	  System.out.println("invoked start");
+	  return false;
+	  
+  }
+}
---
+++ java_06-otc/BikeRunner.java
@@ -0,0 +1,10 @@
+class BikeRunner{
+	public static void main(String[] args){
+		boolean value = Bike.start();
+		boolean good = true;
+		System.out.println(value==good);
+			
+	
+	}
+
+}
